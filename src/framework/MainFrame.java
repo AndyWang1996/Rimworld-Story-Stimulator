12f94780b61908ce19e8add599cd3d90eaae38ca
AndyWang1996 <yikai.wang@ucdconnect.ie>
2020-04-14 20:47:33 +0100

    frame work #2
    Signed-off-by: AndyWang1996 <yikai.wang@ucdconnect.ie>

--- src/framework/MainFrame.java
+++ src/framework/MainFrame.java
@@ -5,12 +5,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.DebugGraphics;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 
 import objects.Human;
 
@@ -32,53 +39,112 @@ public class MainFrame {
 	private JButton badButton;
 	private JButton continueButton;
 	
+	private JScrollPane clistJScrollPane;
+	
+	protected ActionListener clickActionListener;
+	
 	private MainFrame() {
 		
 		mainFrame = new JFrame("Keeper");
 		
 		leftJPanel = new JPanel();
-		leftJPanel.setLayout(new GridLayout(2,1));
+		leftJPanel.setLayout(new BorderLayout());
+		leftJPanel.setPreferredSize(new Dimension(780,600));
+//		leftJPanel.setSize(new Dimension(400,600));
 		rightJPanel = new JPanel();
-		rightJPanel.setLayout(new GridLayout(1,1));;
+		rightJPanel.setLayout(new BorderLayout());
+		rightJPanel.setPreferredSize(new Dimension(200,600));
+//		rightJPanel.setSize(new Dimension(100,600));
 		
 		textPanel = new JPanel();
 		stroyTextArea = new JTextArea();
 		stroyTextArea.setEditable(false);
 		stroyTextArea.setVisible(true);
 		stroyTextArea.setForeground(Color.blue);
-		textPanel.setLayout(new GridLayout(1,1));
+		textPanel.setLayout(new BorderLayout());
+		textPanel.setPreferredSize(new Dimension(700,450));
 		textPanel.add(stroyTextArea);
+		textPanel.setBorder(new TitledBorder("Story"));
 		textPanel.setVisible(true);
 		
 		characterPanel = new JPanel();
 		characterList = new JList<Human>();
+		
 		characterPanel.setLayout(new GridLayout(1,1));
-		characterPanel.add(characterList);
+		clistJScrollPane = new JScrollPane(characterList);
+		characterPanel.add(clistJScrollPane);
+		characterPanel.setBorder(new TitledBorder("Characters"));
 		characterPanel.setVisible(true);
 		
 		buttonPanel = new JPanel();
 		goodButton = new JButton("GOOD");
 		badButton = new JButton("BAD");
 		continueButton = new JButton("GO ON");
-		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setLayout(new GridLayout(1,3));
+		buttonPanel.setPreferredSize(new Dimension(700,125));
+		buttonPanel.setBorder(new TitledBorder("Selections"));
 		buttonPanel.add(goodButton);
 		buttonPanel.add(badButton);
 		buttonPanel.add(continueButton);
 		buttonPanel.setVisible(true);
 		
-		leftJPanel.add(textPanel);
-		leftJPanel.add(buttonPanel);
-		leftJPanel.setPreferredSize(new Dimension(400,600));
+		leftJPanel.add(textPanel,BorderLayout.NORTH);
+		leftJPanel.add(buttonPanel,BorderLayout.SOUTH);
 		rightJPanel.add(characterPanel);
 		
-		mainFrame.setLayout(new GridLayout(1,2));
-		mainFrame.add(leftJPanel);
-		mainFrame.add(rightJPanel);
-		mainFrame.setSize(500,600);
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.add(leftJPanel,BorderLayout.WEST);
+		mainFrame.add(rightJPanel,BorderLayout.EAST);
+		mainFrame.setSize(1000,618);
+		
+		clickActionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				System.out.print(e + "\n");
+				if (e.getActionCommand().equals("GOOD")) {
+					Do_sth_good();
+				}
+				else if (e.getActionCommand().equals("BAD")) {
+					Do_sth_bad();
+				}
+				else if (e.getActionCommand().equals("GO ON")) {
+					Do_sth_normal();
+				}
+			}
+
+		};
+		
+		badButton.addActionListener(clickActionListener);
+		goodButton.addActionListener(clickActionListener);
+		continueButton.addActionListener(clickActionListener);
 		
 		mainFrame.setVisible(true);
 		
 	}
+	
+	private void Do_sth_normal() {
+		// TODO Display the story
+//		stroyTextArea.append("normal");
+
+		stroyTextArea.append("\n");
+		
+	}
+
+	private void Do_sth_bad() {
+		// TODO Display the story
+//		stroyTextArea.append("bad");
+
+		stroyTextArea.append("\n");
+		
+	}
+
+	private void Do_sth_good() {
+		// TODO Display the story
+//		stroyTextArea.append("good");
+
+		stroyTextArea.append("\n");
+		
+	}
+	
 	
 	public static void main(String args[]) {
 		new MainFrame();
