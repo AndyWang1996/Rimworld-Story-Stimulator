7a1c29914716e3514938d0175ecaf1f44329674b
AndyWang1996 <yikai.wang@ucdconnect.ie>
2020-04-19 22:10:10 +0100

    Dice part start
    Signed-off-by: AndyWang1996 <yikai.wang@ucdconnect.ie>

---
+++ src/fitness/Dice.java
@@ -0,0 +1,14 @@
+package fitness;
+
+import java.util.Random;
+
+public class Dice {
+	
+	private Random random;
+	
+	public int throw_a_dice(String command) {
+		random = new Random(System.currentTimeMillis());
+		int result = 0;
+		return result;
+	}
+}
