7fa90e5edc42d0e7931ab9dfa773abc169546389
AndyWang1996 <yikai.wang@ucdconnect.ie>
2020-05-02 15:55:35 +0100

    Signed-off-by: AndyWang1996 <yikai.wang@ucdconnect.ie>

---
+++ src/fitness/Scenes.java
@@ -0,0 +1,20 @@
+package fitness;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import objects.Human;
+
+public class Scenes {
+	List<Human> subjectHumans = new ArrayList<>();
+	List<Human> objectHumans = new ArrayList<>();
+	Map<Human, List<String>> socialMotivationMap = new HashMap<Human, List<String>>();
+	
+	public Scenes(List<Human> subjectHumans, List<Human> objectHumans) {
+		this.subjectHumans = subjectHumans;
+		this.objectHumans = objectHumans;
+	}
+	
+}
--- src/objects/Relation.java
+++ src/objects/Relation.java
@@ -1,6 +1,55 @@
 package objects;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import com.alibaba.fastjson.JSON;
+
+import database.DataLoader;
+
 public class Relation {
 	String relationNameString;
-	Status relationStatu;
+	Status relationStatus;
+	
+	public Relation() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Map<String, Status> generate_good_relations_list() {
+		Map result = new HashMap<String, Object>();
+		result = (Map) ((Map) JSON.parse(DataLoader.readJsonFile(new File("./data/humans/GoodRelation.json")))).get("GoodRelations");
+		return load_loop(result);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Map<String, Status> generate_bad_relations_list() {
+		Map result = new HashMap<String, Object>();
+		result = (Map) ((Map) JSON.parse(DataLoader.readJsonFile(new File("./data/humans/BadRelation.json")))).get("BadRelations");
+		return load_loop(result);
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Map<String,Status> load_loop(Map<String, Object> rawMap){
+		Map result = new HashMap<String, Status>();
+		for(String Key : rawMap.keySet()) {
+			Status status = new Status(
+					(String) ((Map)rawMap.get(Key)).get("name"), 
+					(String) ((Map)rawMap.get(Key)).get("detail"), 
+					"", 
+					(int) ((Map)rawMap.get(Key)).get("time"), 
+					(String) ((Map)rawMap.get(Key)).get("GoodOrBad")
+			);
+			result.put(((Map) rawMap.get(Key)).get("name").toString(), status);		
+		}
+		return result;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(generate_bad_relations_list());
+		System.out.println(generate_good_relations_list());
+	}
 }
