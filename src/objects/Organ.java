18e114c57d07352e696d09aabc948a6ac42f9faf
AndyWang1996 <yikai.wang@ucdconnect.ie>
2020-05-02 06:04:55 +0100

    Signed-off-by: AndyWang1996 <yikai.wang@ucdconnect.ie>

--- src/objects/Organ.java
+++ src/objects/Organ.java
@@ -1,10 +1,61 @@
 package objects;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+
+import database.DataLoader;
+
 public class Organ {
 	
-	public static String organ_name;
+	public String organ_name;
 	public boolean is_lethal;
 	public Status status;
 	
+	public Organ(String name, boolean is_lethal, Status status) {
+		this.organ_name = name;
+		this.is_lethal = is_lethal;
+		this.status = status;
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static List<Organ> create_flesh(){
+		List<Organ> fleshList = new ArrayList<Organ>();
+		Map tempMap = new HashMap<String, List<String>>();
+		Status healthStatus = Status.generate_body_status_list().get("health");
+		tempMap = (Map) JSON.parse(DataLoader.readJsonFile(new File("./data/humans/bodyParts_modified.json")));
+		List<String> tempList = ((List<String>)tempMap.get("bodyParts"));
+//		System.out.println(tempList);
+		for (int i = 0; i < tempList.size(); i++) {
+			fleshList.add(i,new Organ(
+					tempList.get(i).toString(),
+					false, 
+					healthStatus));
+		}
+		tempList = ((List<String>)tempMap.get("BackParts"));
+		for (int i = 0; i < tempList.size(); i++) {
+			fleshList.add(i,new Organ(
+					tempList.get(i).toString(),
+					false, 
+					healthStatus));
+		}
+		tempList = ((List<String>)tempMap.get("LethalParts"));
+		for (int i = 0; i < tempList.size(); i++) {
+			fleshList.add(i,new Organ(
+					tempList.get(i).toString(),
+					true, 
+					healthStatus));
+		}
+//		System.out.println(fleshList.get(3).status.get_detail());
+		return fleshList;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(create_flesh());
+	}
 
 }
--- src/objects/Weapon.java
+++ src/objects/Weapon.java
@@ -1,5 +1,36 @@
 package objects;
 
-public class Weapon {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+
+import database.DataLoader;
+import fitness.Dice;
+
+public class Weapon {
+	
+	String nameString;
+	
+	public Weapon(String name) {
+		this.nameString = name;
+	}
+	
+	public String getNameString() {
+		return nameString;
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static Weapon getWeapon() {
+		List<String> weaponList = new ArrayList<String>();
+		weaponList = (List<String>) ((Map) JSON.parse(DataLoader.readJsonFile(new File("./data/technology/guns_n_rifles.json")))).get("weapons");
+		System.out.println(weaponList);
+		return new Weapon(weaponList.get(Dice.throw_a_dice("1d"+weaponList.size())));
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(getWeapon().getNameString());
+	}
 }
